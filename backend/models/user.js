dd403c2fb8005b0da5bde02afd93ee91266c8c17
Antonin-crypto <akamis53@gmail.com>
2025-07-29 15:27:27 +0200

    add last_name field to User model

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 
 const User = sequelize.define("User", {
   name: { type: DataTypes.STRING, allowNull: false },
+  last_name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
   role: {
