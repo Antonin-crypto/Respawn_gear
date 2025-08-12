d302ed2205a8f0a3a7a64279174c291cafd629a8
Antonin-crypto <akamis53@gmail.com>
2025-08-12 10:38:19 +0200

    setup app with routes, cookie handling, CORS, user and product modules

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const authRoutes = require("./routes/auth");
+const loginRoutes = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const produitRoutes = require("../backend/routes/produits");
+const checkRole = require("../backend/middleware/checkRole");
+const authenticateToken = require("../backend/middleware/authenticateToken");
 
 app.use(express.json());
 
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 app.use("/api", authRoutes);
+app.use("/api/users", loginRoutes);
+
+app.use("/produits", authenticateToken, checkRole(["admin"]), produitRoutes);
 
 app.use("/", (req, res) => {
   res.send("bienvenue");
