02e0e5721fcdd1d0da17136d55e12a7363334e31
Antonin-crypto <akamis53@gmail.com>
2025-08-12 10:56:54 +0200

    update main app files and styles and dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import SignUp from "./pages/SignUp";
+import LoginPage from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import Produit from "./pages/Produit";
+import ProductForm from "./pages/composent/product/ProductForm";
+import CartPage from "./pages/Cart";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/sign_up" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit_profile" element={<EditProfile />} />
+        <Route path="/produit" element={<Produit />} />
+        <Route path="/produits/nouveau" element={<ProductForm />} />
+        <Route path="/produits/modifier/:id" element={<ProductForm />} />
+        <Route path="/panier" element={<CartPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
