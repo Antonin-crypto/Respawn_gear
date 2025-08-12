27514c0ea729da0064985c058194c8076291a447
Antonin-crypto <akamis53@gmail.com>
2025-08-12 10:55:55 +0200

    update auth, cart, and product contexts

---
+++ frontend/src/pages/contexts/AuthContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    const axiosUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/api/users/profile", {
+          withCredentials: true,
+        });
+        setUser(res.data);
+      } catch (error) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    axiosUser();
+  }, []);
+
+  const logout = async () => {
+    await axios.post("http://localhost:5000/api/users/logout", {
+      withCredentials: true,
+    });
+    setUser(null);
+  };
+
+  const deleteAccount = async () => {
+    if (!window.confirm("Es-tu sûr de vouloit supprimer ton compte ?")) return;
+
+    try {
+      const res = await axios.delete(
+        "http://localhost:5000/api/users/profile/delete",
+        {
+          withCredentials: true,
+        }
+      );
+      alert(res.data.message);
+      setUser(null);
+      window.location.href = "/login";
+    } catch (err) {
+      console.error(err);
+      alert("Erreur lords de la suppression du compte.");
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{ user, setUser, loading, logout, deleteAccount }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ frontend/src/pages/contexts/CartContext.jsx
@@ -0,0 +1,58 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (product) => {
+    console.log("🛒 Produit ajouté au panier :", product);
+    setCart((prevCart) => {
+      const existing = prevCart.find((item) => item.id === product.id);
+      if (existing) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
+  const updateQuantity = (id, quantity) => {
+    if (quantity <= 0) {
+      removeFromCart(id);
+    } else {
+      setCart((prevCart) =>
+        prevCart.map((item) => (item.id === id ? { ...item, quantity } : item))
+      );
+    }
+  };
+
+  const clearCart = () => setCart([]);
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, updateQuantity, clearCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
