718bd5523647cf0621e35e85c57e40998b4c48ed
Antonin-crypto <akamis53@gmail.com>
2025-08-18 10:17:25 +0200

    Update check_produit to restrict access to admin

--- backend/middleware/check_produit.js
+++ backend/middleware/check_produit.js
@@ -2,18 +2,21 @@ const Produit = require("../models/produit");
 
 const checkOwnership = async (req, res, next) => {
   try {
+    if (req.user.role !== "admin") {
+      return res.status(403).json({
+        message: "Seuls les administrateurs peuvent effectuer cette action",
+      });
+    }
+
     const produit = await Produit.findByPk(req.params.id);
     if (!produit) {
       return res.status(404).json({ message: "Produit non trouvé" });
     }
 
-    if (produit.userId !== req.user.id) {
-      return res.status(403).json({ message: "Accès refusé" });
-    }
-
     req.produit = produit;
     next();
   } catch (error) {
+    console.error(error);
     return res.status(500).json({ message: "Erreur serveur" });
   }
 };
