58aa2f6cc98aa546b37aeea55858d014530830e3
Antonin-crypto <akamis53@gmail.com>
2025-09-01 11:15:41 +0200

    add app fronted route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,22 +10,55 @@ import Produit from "./pages/Produit";
 import ProductForm from "./pages/composent/product/ProductForm";
 import CartPage from "./pages/Cart";
 import Home from "./pages/Home";
+import ProtectedRoute from "../src/pages/composent/admin/ProtectedRoute";
+import ProductId from "./pages/composent/product/ProductId";
+import { CartProvider } from "./pages/contexts/CartContext";
+import Page404 from "./pages/page404";
+import CategoryPage from "./pages/CategoryPage";
+import CheckoutPage from "./pages/CheckoutPage";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/sign_up" element={<SignUp />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/edit_profile" element={<EditProfile />} />
-        <Route path="/produit" element={<Produit />} />
-        <Route path="/produits/nouveau" element={<ProductForm />} />
-        <Route path="/produits/modifier/:id" element={<ProductForm />} />
-        <Route path="/panier" element={<CartPage />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="*" element={<Page404 />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/category/:categoryName" element={<CategoryPage />} />
+          <Route path="/sign_up" element={<SignUp />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit_profile" element={<EditProfile />} />
+          <Route
+            path="/produit"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <Produit />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/produits/nouveau"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <ProductForm />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/produits/:id" element={<ProductId />} />
+          <Route
+            path="/produits/modifier/:id"
+            element={
+              <ProtectedRoute allowedRoles={["admin"]}>
+                <ProductForm />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/panier" element={<CartPage />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
 
